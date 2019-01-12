6206d2ad1a6d1d349f4da79096b6397be83d3f05
BogdanMateratskyy <bogdan.materatskyy@gmail.com>
2019-01-12 15:56:01 -0500

    fixed some error

--- prediction_into_hbase.sh
+++ prediction_into_hbase.sh
@@ -2,4 +2,4 @@
 
 PREDICT_LOCAL_PATH='/home/cloudera/practice/task20a/predict_iris.csv'
 hadoop fs -put -f ${PREDICT_LOCAL_PATH} predict_iris_hdfs.csv
-hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator="," -Dimporttsv.columns=HBASE_ROW_KEY,cf:Iris setosa,cf:Iris versicolor,cf:Iris virginica iris_predict predict_iris_hdfs.csv
+hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator="," -Dimporttsv.columns=HBASE_ROW_KEY,cf:Iris_setosa,cf:Iris_versicolor,cf:Iris_virginica iris_predict predict_iris_hdfs.csv
